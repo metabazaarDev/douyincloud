3f511465ffc551b6d19aa402e01d6c881738a253
frankin5 <frankin5@sina.com>
2023-11-03 14:22:25 +0800

    add cors  https://cloud.douyin.com

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -5,7 +5,7 @@ import cors from "@koa/cors";
 
 export default async function(ctx: Koa.Context,next: Koa.Next){
     const corsOptions = {
-        origin: ["https://metabazaar.com.cn"], // 允许的域名
+        origin: ["https://metabazaar.com.cn","https://cloud.douyin.com"], // 允许的域名
       };
 
       cors(corsOptions);
